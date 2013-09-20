1d4929ee7bbce4c2596d9a672a0cd1486bd1d35a
Rochas <dresscording@gmail.com>
2013-09-20 19:17:24 +0900

    change directry

--- config.rb
+++ config.rb
@@ -2,21 +2,20 @@
 # Blog settings
 ###
 
-# Time.zone = "UTC"
+Time.zone = "Tokyo"
 
 activate :blog do |blog|
-  Time.zone = "Tokyo"
   blog.prefix = "blog"
-  blog.permalink = ":title.html"
+  # blog.permalink = ":title.html"
   # blog.sources = ":year-:month-:day-:title.html"
-  blog.taglink = "tags/:tag.html"
+  blog.taglink = ":tag.html"
   blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
   blog.summary_length = 200
   # blog.year_link = ":year.html"
   # blog.month_link = ":year/:month.html"
   # blog.day_link = ":year/:month/:day.html"
-  # blog.default_extension = ".markdown"
+  blog.default_extension = ".md"
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
   blog.summary_separator = /SPLIT_SUMMARY_BEFORE_THIS/
---
+++ source/blog/2013-09-02-nice-middleman.md
@@ -0,0 +1,15 @@
+---
+title: ブログをリプロデュースしました
+date: 2013-09-02
+tags: Test
+layout: post
+---
+
+* Middleman
+* SMACSS
+* Vertical Rythm
+* main 要素
+* WAI ARIA [Land Mark]
+
+
+
