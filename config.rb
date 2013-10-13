cc46c7ab2f07f48fb4d8f7d58c30ce305b0c0372
Rochas <dresscording@gmail.com>
2013-10-13 16:39:32 +0900

    font fix

--- config.rb
+++ config.rb
@@ -19,7 +19,7 @@ activate :blog do |blog|
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
   blog.summary_separator = /SPLIT_SUMMARY_BEFORE_THIS/
-  # blog.paginate = true
+  blog.paginate = true
   blog.per_page = 30
   # blog.page_link = "page/:num"
 end
