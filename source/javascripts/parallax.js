650a905129ea3065ffdb750f779259c9b0e92b5d
Rochas <dresscording@gmail.com>
2013-10-24 16:07:03 +0900

    change .logo-wrapper

--- source/javascripts/parallax.js
+++ source/javascripts/parallax.js
@@ -3,7 +3,7 @@
     if($(window).width()>760){
       $(document).scroll(function(){
           val1 = 1.05 - ( ($(document).scrollTop()) / ($('.header').height()) );
-          $('.flip').css('opacity', val1);
+          $('.logo-wrapper').css('opacity', val1);
           val2= -1 + ( ($(document).scrollTop()) /  $(window).height()+0.8 );
           $('.footer p').css('opacity', val2);
       });
