6921d1117987f24a799bd693504bd4a5e7e05061
Rochas <dresscording@gmail.com>
2013-10-28 00:03:07 +0900

    activate :minify_css

--- config.rb
+++ config.rb
@@ -104,7 +104,7 @@ set :partials_dir, 'partials'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
